f0d04e88ef8beedf9b65eb377c378e3cbcad0282
akbarbudiman <43605016+akbarbudiman@users.noreply.github.com>
2021-05-06 10:10:35 +0700

    SameTree

---
+++ src/main/java/binarytree/dfs/SameTree.java
@@ -0,0 +1,26 @@
+package src.main.java.binarytree.dfs;
+
+import src.main.java.binarytree.TreeNode;
+
+
+
+public class SameTree {
+
+	/*
+	 *  origin:
+	 *  https://leetcode.com/problems/same-tree/
+	 *  
+	 *  given:
+	 *  two ThreeNode : p and q
+	 *  
+	 *  result:
+	 *  whether p and q are the same (structurally identical, and the nodes have the same value)
+	 */
+	
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        else if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    
+}
---
+++ src/main/java/dynamicprogramming/tabulation/CanSumTabulation.java
@@ -0,0 +1,10 @@
+package src.main.java.dynamicprogramming.tabulation;
+
+public class CanSumTabulation {
+
+	/*
+	 * 3:38:00
+	 * https://youtu.be/oBt53YbR9Kk
+	 */
+	
+}
